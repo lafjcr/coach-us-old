a90a1d5b34841cfd805ed384bc6dd87d51be3f69
lafj14 <lafj14@gmail.com>
2013-10-22 16:55:01 +0000

    Adding manager classes

---
+++ CoachUs.Managers/BaseManager.cs
@@ -0,0 +1,24 @@
+﻿using CoachUs.Data;
+using System;
+
+namespace CoachUs.Managers
+{
+    public abstract class BaseManager : IDisposable
+    {
+        protected CoachUsEntities context = null;
+
+        public BaseManager() : this(null)
+        {
+        }
+
+        internal BaseManager(CoachUsEntities context = null)
+        {
+            this.context = context ?? new CoachUsEntities();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ CoachUs.Managers/SecurityManager.cs
@@ -0,0 +1,22 @@
+﻿using CoachUs.Data.Entities;
+using System;
+using System.Linq;
+
+namespace CoachUs.Managers
+{
+    public class SecurityManager : BaseManager
+    {
+        #region Roles
+        public IOrderedQueryable<Role> GetRoles()
+        {
+            return context.Roles.OrderBy(item => item.Name);
+        }
+        #endregion
+
+        #region Permissions
+        #endregion
+
+        #region Actions
+        #endregion
+    }
+}

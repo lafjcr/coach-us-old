3748204056cf12f0df66f7d823312ef354ae6209
lafj14 <lafj14@gmail.com>
2013-12-06 21:46:35 +0000

    Enable Google authentication

--- CoachUs.Site/App_Start/Startup.Auth.cs
+++ CoachUs.Site/App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace CoachUs.Site
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }

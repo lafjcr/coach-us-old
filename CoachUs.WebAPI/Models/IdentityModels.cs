62c33a7338eb89adab0c163802a3e2829d9f28dc
lafjcr <lafj14@gmail.com>
2015-06-01 18:22:24 -0600

    Update projects to allow create web api controller

--- CoachUs.WebAPI/Models/IdentityModels.cs
+++ CoachUs.WebAPI/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace CoachUs.WebAPI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("CoachUs", throwIfV1Schema: false)
         {
         }
         
